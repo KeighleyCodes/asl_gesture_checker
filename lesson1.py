2a380b7747e3dd212d06f30be3ab3639f365bfe0
Keighley Manke <keighley.manke@gmail.com>
2024-05-13 14:08:07 -0700

    Completed project

--- lesson1.py
+++ lesson1.py
@@ -45,7 +45,8 @@ def lesson_page_1():
 
         # Load model
         try:
-            lesson1_model = load_model('lesson1.keras')
+            lesson1_model = load_model('Models/lesson1.keras')
+
         except Exception as e:
             st.error(f"Error loading the model: {e}")
             st.error(f"Exception traceback: {traceback.format_exc()}")
