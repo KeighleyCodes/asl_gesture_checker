376c0a8c95155aaa71d9097172d340aae92e4379
Keighley Manke <keighley.manke@gmail.com>
2024-05-16 14:34:04 -0700

    Errors loading keras files on deploy

--- lesson1.py
+++ lesson1.py
@@ -40,7 +40,7 @@ def lesson_page_1():
 
         # Load model
         try:
-            lesson1_model = load_model('lesson1.keras')
+            lesson1_model = load_model('lesson1.keras', compile=False)
         except Exception as e:
             st.error(f"Error loading the model: {e}")
             st.error(f"Exception traceback: {traceback.format_exc()}")
