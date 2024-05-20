e32b20758d13d2a2811bcea30b03b3c7e79a6712
Keighley Manke <keighley.manke@gmail.com>
2024-05-20 09:11:51 -0700

    Completed project

--- lesson1.py
+++ lesson1.py
@@ -3,50 +3,30 @@ import streamlit as st
 import cv2
 import numpy as np
 import mediapipe as mp
-import tensorflow as tf
-import traceback
 from gcsfs import GCSFileSystem
 from streamlit_webrtc import (webrtc_streamer, VideoProcessorBase, WebRtcMode, RTCConfiguration)
-from shared_functions import (mediapipe_detection, extract_key_points, display_gif, display_gesture_checkboxes)
+from shared_functions import (mediapipe_detection, extract_key_points, display_gif, display_gesture_checkboxes,
+                              download_and_load_model)
 
 # Initialize a Mediapipe Holistic object
 mp_holistic = mp.solutions.holistic
-
-
-# Define a function to download and load models directly from GCS
-@st.cache_data(ttl=600)  # Cache with a time-to-live (TTL) of 600 seconds (10 minutes)
-def download_and_load_model(_fs, model_path):  # Add a leading underscore to fs
-    try:
-        with _fs.open(model_path, 'rb') as f:  # Use _fs instead of fs
-            model = tf.keras.models.load_model(f, compile=False)
-        return model
-    except FileNotFoundError as e:
-        st.error(f"Model file not found: {model_path}")
-    except ValueError as e:
-        st.error(f"Error loading the model: {e}")
-    except OSError as e:
-        st.error(f"OS error: {e}")
-    except Exception as e:
-        st.error(f"An unexpected error occurred: {e}")
-
 
 # Initialize a GCS file system object
 fs = GCSFileSystem(project='keras-file-storage')
 
 # Specify the path to the model file in the GCS bucket
 model_path = 'gs://keras-files/lesson1.keras'
+local_model_path = 'lesson1.keras'
 
-# Call the function to download and load the model
-lesson1_model = download_and_load_model(fs, model_path)
+# Call function to download the model
+lesson1_model = download_and_load_model(model_path, local_model_path)
 
 
 # Define a custom video processor class inheriting from VideoProcessorBase
 class VideoProcessor(VideoProcessorBase):
     def __init__(self):
         self.model = lesson1_model  # Initialize model attribute with loaded model
-        self.actions = np.array(
-            ['again', 'alive', 'dad', 'family', 'friend', 'hard_of_hearing', 'help_me', 'how', 'hungry',
-             'like'])  # Define action labels
+        self.actions = np.array(['again', 'alive', 'dad', 'family', 'friend', 'hard_of_hearing', 'help_me', 'how', 'hungry', 'like'])  # Define action labels
         self.sequence = []  # Initialize an empty list to store key point sequences
         self.sentence = []  # Initialize an empty list to store recognized sentences
         self.threshold = 0.4  # Define a confidence threshold
@@ -68,8 +48,7 @@ class VideoProcessor(VideoProcessorBase):
                 res = self.model.predict(np.expand_dims(self.sequence, axis=0))[0]  # Perform model prediction
                 predicted_action_index = np.argmax(res)  # Get index of predicted action
                 if res[predicted_action_index] > self.threshold:  # Check if prediction confidence is above threshold
-                    self.sentence.append(
-                        self.actions[predicted_action_index])  # Append predicted action to sentence list
+                    self.sentence.append(self.actions[predicted_action_index])  # Append predicted action to sentence list
 
             if len(self.sentence) > 5:  # Keep only the last 5 sentences
                 self.sentence = self.sentence[-5:]
@@ -79,7 +58,6 @@ class VideoProcessor(VideoProcessorBase):
 
         # Convert the modified image back to a video frame and return it
         return av.VideoFrame.from_ndarray(image, format="bgr24")
-
 
 # Define RTC configuration for WebRTC
 RTC_CONFIGURATION = RTCConfiguration({
@@ -114,7 +92,7 @@ def lesson_page_1():
         if selected:
             display_gif(gif_path=gesture_gifs[gesture_name], gesture_name=gesture_name)
 
-        # Start WebRTC streaming session
+    # Start WebRTC streaming session
     webrtc_streamer(
         key="example",
         mode=WebRtcMode.SENDRECV,
